a6aaa3f2d00d8610930818d94c55c2bcfc6e9d69
sliman <73777714+sliman-lan@users.noreply.github.com>
2026-03-09 19:36:32 +0300

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -68,7 +68,7 @@ app.use((err, req, res, next) => {
 
 // تشغيل الخادم
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT,'0.0.0.0', () => {
     console.log(`✅ الخادم يعمل على http://localhost:${PORT}`);
     console.log(`📁 قاعدة البيانات: ${process.env.MONGODB_URI}`);
 });
